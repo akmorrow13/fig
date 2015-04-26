4c38c81567016ede27ebda0fba4bd0817c2da2e3
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2015-04-26 09:45:12 -0700

    Adding models and model loading code.

--- src/main/scala/net/fnothaft/fig/Fig.scala
+++ src/main/scala/net/fnothaft/fig/Fig.scala
@@ -16,17 +16,15 @@
 package net.fnothaft.fig
 
 import java.io.File
+import net.fnothaft.fig.models._
 import org.apache.hadoop.mapreduce.Job
 import org.apache.spark.SparkContext._
 import org.apache.spark.{ Logging, SparkContext }
-import org.bdgenomics.adam.models.ReferenceRegion
 import org.bdgenomics.adam.rdd.ADAMContext._
-import org.bdgenomics.adam.rdd.BroadcastRegionJoin
+import org.bdgenomics.adam.util.TwoBitFile
 import org.bdgenomics.formats.avro._
 import org.bdgenomics.utils.cli._
-import org.bdgenomics.utils.parquet.rdd.BDGParquetContext._
-import org.kitesdk.data.{ DatasetDescriptor, Datasets, Formats, View }
-import org.kitesdk.data.mapreduce.DatasetKeyOutputFormat
+import org.bdgenomics.utils.parquet.io.LocalFileByteAccess
 import org.kohsuke.args4j.{ Argument, Option => Args4jOption }
 import parquet.avro.AvroReadSupport
 import parquet.hadoop.ParquetInputFormat
@@ -45,11 +43,20 @@ class FigArgs extends Args4jBase {
   @Argument(required = true, metaVar = "GENOTYPES", usage = "The genotypes to process.", index = 0)
   var genotypes: String = null
 
-  @Argument(required = true, metaVar = "CONTIGS", usage = "The reference genome.", index = 1)
+  @Argument(required = true, metaVar = "REFERENCE", usage = "A 2bit file for the reference genome.", index = 1)
   var contigs: String = null
 
   @Argument(required = true, metaVar = "GENES", usage = "The gene description file.", index = 2)
   var genes: String = null
+
+  @Argument(required = true, metaVar = "MOTIFS", usage = "A file describing TF motifs.", index = 3)
+  var motifs: String = null
+
+  @Argument(required = true, metaVar = "SITES", usage = "A feature file describing TFBSs.", index = 4)
+  var sites: String = null
+
+  @Argument(required = true, metaVar = "OUTPUT", usage = "Where to write output to.", index = 5)
+  var outputPath: String = null
 
   @Args4jOption(required = false, name = "-before_tss", usage = "The distance to start considering before the TSS. Default is 1500.")
   var startBeforeTss: Int = 1500
@@ -62,6 +69,20 @@ class Fig(protected val args: FigArgs) extends BDGSparkCommand[FigArgs] {
   val companion = Fig
 
   def run(sc: SparkContext, job: Job) {
+
+    // load two bit file
+    val tbf = new TwoBitFile(new LocalFileByteAccess(new File(args.contigs)))
+
+    // turn these features into structures
+    val structures = ReferencePromoter(sc.loadGenes(args.genes),
+                                       sc.textFile(args.sites),
+                                       tbf,
+                                       args.startBeforeTss,
+                                       args.stopBeforeTss)
+
+    // load in motifs
+    val motifs = Motif(args.motifs)
+
     ???
   }
 }
